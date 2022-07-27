0e1e043f3b94d0410a7bceec2fbd166f9a136946
liamsquire <liamsquire@gmail.com>
2022-07-27 09:45:09 +0100

    Updated CSS to use Google font and added placeholder text to input fields

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,13 @@ import troll from '../images/troll-face.png'
 
 export default function Meme() {
     return (
-        <section className="meme--container">
-            <div className="grid-container">
-                <div><input type="text" id="meme-start-text" /></div>
-                <div><input type="text" id="meme-end-text" /></div>
+        <section className="meme--container"> 
+            <form className="grid-container">
+                <div><input type="text" id="meme-start-text" placeholder="Top text"/></div>
+                <div><input type="text" id="meme-end-text" placeholder="Bottom text"/></div>
                 <div className="button--meme-image"><button>Get a new Meme image 🖼️</button></div>
                 <div className="meme--image"><img src={troll} alt="Troll face" /></div>
-            </div>
+            </form>
         </section>      
     )
 }
